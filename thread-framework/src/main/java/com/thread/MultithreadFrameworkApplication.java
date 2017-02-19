b071c0da7b99ad6ebf17f7d476e98d60a418a219
harshbhavsar <harsh.bhavsar@copart.com>
2017-02-19 17:06:08 -0600

    Initial Commit

---
+++ thread-framework/src/main/java/com/thread/MultithreadFrameworkApplication.java
@@ -0,0 +1,12 @@
+package com.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadFrameworkApplication.class, args);
+	}
+}
