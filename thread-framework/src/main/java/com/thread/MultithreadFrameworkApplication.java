aa6bbf9cc018b3976d1922c1e33d214b41d0ab19
vaishsh <vaishali.shah@copart.com>
2017-02-19 17:22:49 -0600

    esdfghj

--- thread-framework/src/main/java/com/thread/MultithreadFrameworkApplication.java
+++ thread-framework/src/main/java/com/thread/MultithreadFrameworkApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MultithreadFrameworkApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("Bhad me jaa");
 		SpringApplication.run(MultithreadFrameworkApplication.class, args);
 	}
 }
