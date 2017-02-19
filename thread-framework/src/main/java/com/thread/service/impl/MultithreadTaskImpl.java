1605a34f96f3c8667fa83e926c13defd34dab588
harshbhavsar <harsh.bhavsar@copart.com>
2017-02-19 17:38:32 -0600

    h<3v

--- thread-framework/src/main/java/com/thread/service/impl/MultithreadTaskImpl.java
+++ thread-framework/src/main/java/com/thread/service/impl/MultithreadTaskImpl.java
@@ -7,6 +7,8 @@ public class MultithreadTaskImpl implements MultithreadTask {
 	@Override
 	public void performTask() {
 		System.out.println("Perform ");
+		
+		System.out.println("i lv u");
 	}
 
 }
