45f5f12fba9518cafc10aacc1aaefa9510499ab6
vaishsh <vaishali.shah@copart.com>
2017-02-19 18:14:04 -0600

    added files

--- thread-framework/src/main/java/com/thread/MultithreadFrameworkApplication.java
+++ thread-framework/src/main/java/com/thread/MultithreadFrameworkApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MultithreadFrameworkApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
 		SpringApplication.run(MultithreadFrameworkApplication.class, args);
 	}
 }
--- thread-framework/src/main/java/com/thread/service/impl/MultithreadTaskImpl.java
+++ thread-framework/src/main/java/com/thread/service/impl/MultithreadTaskImpl.java
@@ -6,9 +6,7 @@ public class MultithreadTaskImpl implements MultithreadTask {
 
 	@Override
 	public void performTask() {
-		System.out.println("Perform ");
 		
-		System.out.println("i lv u");
 	}
 
 }
